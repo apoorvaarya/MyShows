7eaa2a8df0c4bcc1855b1f4d4afacb0b5029e4ad
Apoorva <apoorvaarya>
2020-10-11 13:22:11 -0400

    commit

--- app/src/main/java/com/example/myshows/SplashActivity.java
+++ app/src/main/java/com/example/myshows/SplashActivity.java
@@ -2,13 +2,46 @@ package com.example.myshows;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.os.CountDownTimer;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class SplashActivity extends AppCompatActivity {
-
+    MediaPlayer mpUkulele;
+    int playing;
     @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        mpUkulele = new MediaPlayer();
+        mpUkulele = MediaPlayer.create(this, R.raw.ukulele);
+
+        CountDownTimer cntr_aCounter = new CountDownTimer(3000, 1000) {
+            public void onTick(long millisUntilFinished) {
+
+                mpUkulele.start();
+            }
+
+            public void onFinish() {
+                //code fire after finish
+                mpUkulele.stop();
+            }
+        };cntr_aCounter.start();
+
+        TimerTask task = new TimerTask() {
+            @Override
+            public void run() {
+                finish();
+                startActivity(new Intent(SplashActivity.this, ItemsListActivity.class));
+            }
+        };
+        Timer opening = new Timer();
+        opening.schedule(task, 5000);
     }
 }
